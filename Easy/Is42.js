a4c5d7b610c9f9c5adff1bd5ce353dbaf40ce0fd
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-07-10 17:48:40 +0300

    Create Is42.js

---
+++ Easy/Is42.js
@@ -0,0 +1,4 @@
+/* Two numbers are given a and b. Return true if one of the two numbers is 42 or the sum of the two numbers.*/
+function is42(a, b) {
+   return a == 42 || b == 42 || a + b == 42; 
+}
