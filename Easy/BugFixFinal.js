cec0bf806c7a1c290ccd39defda8e13521bf8c0d
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-07-11 20:31:55 +0300

    Create BugFixFinal.js

---
+++ Easy/BugFixFinal.js
@@ -0,0 +1,4 @@
+/*A string string is given. Remove any occurrence of 'bug' by 'flower'. Attention: 'bug' can occur several times in the string.*/
+function bugFixFinal(string) {
+  return string.replaceAll("bug", "flower");
+}
