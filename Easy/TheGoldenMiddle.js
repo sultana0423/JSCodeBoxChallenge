4896eb741e35708e9b51e371146d4efb9f18c04c
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-07-12 22:14:38 +0300

    Create TheGoldenMiddle.js

---
+++ Easy/TheGoldenMiddle.js
@@ -0,0 +1,5 @@
+/*Given are two arrays a and b, both have three elements. 
+Return a new array of length 2 containing both middle (index 1) elements of the arrays.*/
+function goldenMiddle(a, b) {
+return [a[1], b[1]];
+}
