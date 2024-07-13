a5d10600ff83b6e11b349e3eeb09254ead68bd4c
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-07-13 22:20:08 +0300

    Create BuildingCar.js

---
+++ Easy/BuildingCar.js
@@ -0,0 +1,15 @@
+/*Create an object with the following characteristics: 
+4 wheels (named wheels as integer), 
+100PS power (named power and integer), 
+3 doors (named doors as integer). 
+Return the built car.*/
+
+function buildingACar() {
+  let car = {
+    wheels: 4,
+    power: 100,
+    doors: 3
+  };
+
+  return car;
+}
