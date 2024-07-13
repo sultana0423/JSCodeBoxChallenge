60b9d7df66584979caef59e89cd43224f3623776
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-07-13 22:02:20 +0300

    Create FirstJSON.js

---
+++ Easy/FirstJSON.js
@@ -0,0 +1,6 @@
+/*Two variables are given key and value. Return these values in JSON notation, but as a string, not an JSON object.*/
+/* Example: {"name": "JSCodebox"}*/
+
+function firstJSON(key, value) {
+  return `{"${key}":"${value}"}`;
+}
