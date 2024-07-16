6e49a2744dae0e1c4b30f83f5be4861e29a4c622
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-07-16 21:20:44 +0300

    Update Hangman.js

--- Medium/Hangman.js
+++ Medium/Hangman.js
@@ -1,1 +1,14 @@
-/*Given is a string hangmanString. Replace every second letter with an underscore and return the result as a string. The first character must be an underscore.*/
+/*Given is a string hangmanString. Replace every second letter with an underscore and return the result as a string. 
+The first character must be an underscore.*/
+function hangman(hangmanString) {
+  let string = '_';
+  for (let i = 1; i < hangmanString.length; i++) {
+    if (i % 2 == 0) {
+      string += '_';
+    } else {
+      string += hangmanString[i];
+    }
+  }
+
+  return string ;
+}
