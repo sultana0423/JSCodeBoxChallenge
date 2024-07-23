e8c62c271eb01781e2ce8d1468f3552e5639637b
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-07-23 21:34:00 +0300

    Update Palindrome.js

--- Medium/Palindrome.js
+++ Medium/Palindrome.js
@@ -1,4 +1,4 @@
-/*A variable pali is given. Return true if it is a palindrome. Otherwise false. 
+/* A variable pali is given. Return true if it is a palindrome. Otherwise false. 
 A palindrome is a word that is read forward and backward, resulting in the same word.*/
 
 function palindrome(pali) {
