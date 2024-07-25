b07d11dc54a0cd8df6bebbbaa90065fb08ebbb40
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-07-25 22:34:01 +0300

    Create passwordSecurity2.js

---
+++ Medium/passwordSecurity2.js
@@ -0,0 +1,22 @@
+/* Given are two strings password and password_repeat. Check if the password is secure by the following critera:
+- both passwords must match
+- password must be at least 8 chars
+- contains at least a number
+- contains at lease an uppercase letter
+- contains at least a lowercase letter
+- contains at least one of these special chars (&$%§-_)
+
+Return true only if all criteria are met, otherwise return false.*/
+
+function checkPassword(password, password_repeat) {
+  
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[&$%\\u00A7-_]).{8,}$/;
+
+   
+    if (password !== password_repeat) {
+        return false;
+    }
+
+    
+    return passwordRegex.test(password);
+}
