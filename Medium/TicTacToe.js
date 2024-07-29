f9fa6969691f370f26b25d8847e4cc79509a7af0
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-07-30 00:08:42 +0300

    Create TicTacToe.js

---
+++ Medium/TicTacToe.js
@@ -0,0 +1,25 @@
+/*Given are three arrays. Each array represents a line from the game Tic Tac Toe. 
+One player has 'X' as character and the other 'O'. 
+Return true if a player has won the game. If no player has won, return 'Tie'.*/
+
+ // let line1 = ['XOX'];
+ // let line2 = ['XXX'];
+ // let line3 = ['OXO'];
+ // console.log(line1[0]);
+ // console.log(line1[0][0]);
+ // console.log(line1[0][0] == line2[0][0]);
+ // let horizantal = line1[0][0] == line1[0][1] && line1[0][0] == line1[0][2];
+ // let vertical = line1[0][0] == line2[0][0] && line1[0][0] == line3[0][0];
+ // let digonal = line1[0][0] == line2[0][1] && line1[0][0] == line3[0][2];
+ // console.log(digonal);
+
+// let line1 = ['XOX'];
+//  let line2 = ['XXX'];
+//  let line3 = ['OXO'];
+ 
+//  let string1 = line1[0];
+//  let string2 = line2[0];
+//  let string3 = line3[0];
+ 
+//  let theArray = [string1, string2, string3];
+//  console.log(theArray);
