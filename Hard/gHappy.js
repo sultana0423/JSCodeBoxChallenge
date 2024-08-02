8871f003a4092944e14059b488421d35daf0e758
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-08-02 23:48:52 +0300

    Update gHappy.js

--- Hard/gHappy.js
+++ Hard/gHappy.js
@@ -1,1 +1,19 @@
 /*A small 'g' is 'happy', but only if a small 'g' follows before or after it. Return true if all g's are happy.*/
+
+function gHappy(string) {
+     if (string.length === 0) {
+        return false;
+    }
+  for (let i = 0; i < string.length; i++) {
+        if (string[i] === 'g') {
+            if ((i > 0 && string[i - 1] === 'g') || (i < string.length - 1 && string[i + 1] === 'g')) {
+              
+                continue;
+            } else {
+                 
+                return false;
+            }
+        }
+    }
+    return true;
+}
