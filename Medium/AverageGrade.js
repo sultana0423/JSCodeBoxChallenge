f5adf60b89c32011dd14edfc84a82f5c4f30f3c3
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-08-05 14:42:31 +0300

    Update AverageGrade.js

--- Medium/AverageGrade.js
+++ Medium/AverageGrade.js
@@ -2,8 +2,8 @@
 Given is the array scores. Return the calculated average rounded to two decimal places. The length of the array will be at least 1.*/
 
 function calculateAverageGrade(scores) {
-   let sumOfValue = 0;
-   let numOfValue = scores.length;
+   let sumOfValues = 0;
+   let numOfValues = scores.length;
  for (let i = 0; i < scores.length; i++) {
      sumOfValue += scores[i];
  }
