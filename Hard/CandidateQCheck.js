8ff4ff8478b0baaca63dad9afa7feb5609f17ffc
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-08-11 23:25:52 +0300

    Create CandidateQCheck.js

---
+++ Hard/CandidateQCheck.js
@@ -0,0 +1,62 @@
+/*Given is an array times and a number totalTime. The array times contains 
+the time taken by a candidate to solve each question in an imaginary coding interview, 
+and totalTime is the total time taken by the candidate to complete the interview.*/
+
+/*The interview has the following criteria:
+The interview contains 8 questions in the order: easy, easy, easy, medium, medium, medium, hard, hard.
+The maximum allowed time for the entire interview, including a buffer time, is 140 minutes.*/
+
+/*The maximum time allowed for each question is as follows:
+Easy questions: 10 minutes each.
+Medium questions: 15 minutes each.
+Hard questions: 20 minutes each.*/
+
+/*The function should return "qualified" if the candidate meets all the criteria.
+Otherwise, it should return "disqualified".*/
+
+//CODE IS NOT FINISHED
+
+  function isQualified(times, totalTime) {
+     
+    let totalMaxE = 30;
+    let totalMaxM = 45;
+    let totalMaxH = 40;
+    
+    let easyArray = times.slice(0,3);
+    let medArray = times.slice(3,6);
+    let hardArray = times.slice(6);
+    
+    const easySum = (array) => {
+    let easyTotal = 0;
+    for (let i = 0; i < array.length; i++) {
+        easyTotal += array[i];
+    }
+    return easyTotal;
+};
+    const medSum = (array) => {
+    let medTotal = 0;
+    for (let i = 0; i < array.length; i++) {
+        medTotal += array[i];
+    }
+    return medTotal;
+};
+    const hardSum = (array) => {
+    let hardTotal = 0;
+    for (let i = 0; i < array.length; i++) {
+        hardTotal += array[i];
+    }
+    return hardTotal;
+};
+
+    let passedEasy = easySum(easyArray) <= totalMaxE;
+    let passedMed = medSum(medArray) <= totalMaxM;
+    let passedHard = hardSum(hardArray) <= totalMaxH;
+    let passed = passedEasy && passedMed && passedHard;
+    
+    if ((passed && totalTime < 140) || (!passed && totalTime >= 140)) {
+    return "qualified";
+    } else {
+    return "disqualified";
+    }
+    
+}
