38234de768f2ddb0d5666c1045cbebdd93ee5493
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-08-13 00:27:11 +0300

    Update CandidateQCheck.js

--- Hard/CandidateQCheck.js
+++ Hard/CandidateQCheck.js
@@ -14,51 +14,71 @@ Hard questions: 20 minutes each.*/
 /*The function should return "qualified" if the candidate meets all the criteria.
 Otherwise, it should return "disqualified".*/
 
-//CODE IS NOT FINISHED
-  function isQualified(times, totalTime) {
-     
-    let totalMaxE = 30;
-    let totalMaxM = 45;
-    let totalMaxH = 40;
-    
-    let easyArray = times.slice(0,3);
-    let medArray = times.slice(3,6);
-    let hardArray = times.slice(6);
-    
-    const easySum = (array) => {
-    let easyTotal = 0;
-    for (let i = 0; i < array.length; i++) {
-        easyTotal += array[i];
-    }
-    return easyTotal;
-};
-    const medSum = (array) => {
-    let medTotal = 0;
-    for (let i = 0; i < array.length; i++) {
-        medTotal += array[i];
-    }
-    return medTotal;
-};
-    const hardSum = (array) => {
-    let hardTotal = 0;
-    for (let i = 0; i < array.length; i++) {
-        hardTotal += array[i];
-    }
-    return hardTotal;
-};
+function isQualified(times, totalTime) {
+   const maxTimes = [10, 10, 10, 15, 15, 15, 20, 20];
+    const maxTotalTime = 140;
 
-    let passedEasy = easySum(easyArray) <= totalMaxE;
-    let passedMed = medSum(medArray) <= totalMaxM;
-    let passedHard = hardSum(hardArray) <= totalMaxH;
-    let passed = passedEasy && passedMed && passedHard;
-    
-  if ( passed === true && totalTime <= 140) {
-      return 'qualified';
-    }  else if (passed === false && totalTime <= 140) {
-      return 'disqualified';
-    }  else {
-    return 'disqualified';
+    for (let i = 0; i < times.length; i++) {
+        if (times[i] > maxTimes[i]) {
+            return 'disqualified';
+        }
     }
-    
-    
+
+    if (totalTime <= maxTotalTime) {
+        return 'qualified';
+    } else {
+        return 'disqualified';
+    }
 }
+
+
+
+
+ 
+//   function isQualified(times, totalTime) {
+     
+//     let totalMaxE = 30;
+//     let totalMaxM = 45;
+//     let totalMaxH = 40;
+    
+//     let easyArray = times.slice(0,3);
+//     let medArray = times.slice(3,6);
+//     let hardArray = times.slice(6);
+    
+//     const easySum = (array) => {
+//     let easyTotal = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         easyTotal += array[i];
+//     }
+//     return easyTotal;
+// };
+//     const medSum = (array) => {
+//     let medTotal = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         medTotal += array[i];
+//     }
+//     return medTotal;
+// };
+//     const hardSum = (array) => {
+//     let hardTotal = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         hardTotal += array[i];
+//     }
+//     return hardTotal;
+// };
+
+//     let passedEasy = easySum(easyArray) <= totalMaxE;
+//     let passedMed = medSum(medArray) <= totalMaxM;
+//     let passedHard = hardSum(hardArray) <= totalMaxH;
+//     let passed = passedEasy && passedMed && passedHard;
+    
+//   if ( passed === true && totalTime <= 140) {
+//       return 'qualified';
+//     }  else if (passed === false && totalTime <= 140) {
+//       return 'disqualified';
+//     }  else {
+//     return 'disqualified';
+//     }
+    
+    
+// }
