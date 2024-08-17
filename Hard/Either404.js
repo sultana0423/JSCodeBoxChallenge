da1d824854ba6b037970887538571afe340fcf87
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-08-17 21:04:20 +0300

    Update Either404.js

--- Hard/Either404.js
+++ Hard/Either404.js
@@ -1,17 +1,21 @@
 /*Given is an array numbers with integers. Return true if a zero is next to a zero or a four is next to a four. Return false if both occure.*/
 
-//CODE IS NOT FINISHED
 function either404(numbers) {
-  for (let i = 0; i < numbers.length; i++) {
-    let zero = numbers[i] === 0 && numbers[i + 1] === 0;
-    let four = numbers[i] === 4 && numbers[i + 1] === 4;
-    // if (zero && four ) {
-    //   return false;
-    // } else if (zero || four) {
-    //   return true;
-    // }  else {
-    //   return false;
-    // }
-    return zero && four? false : zero || four;
+  let hasZeroPair = false;
+  let hasFourPair = false;
+
+  for (let i = 0; i < numbers.length - 1; i++) {
+    if (numbers[i] === 0 && numbers[i + 1] === 0) {
+      hasZeroPair = true;
+    }
+    if (numbers[i] === 4 && numbers[i + 1] === 4) {
+      hasFourPair = true;
+    }
   }
+
+  if (hasZeroPair && hasFourPair) {
+    return false;
+  }
+
+  return hasZeroPair || hasFourPair;
 }
