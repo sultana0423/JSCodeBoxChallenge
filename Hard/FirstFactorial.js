ba6f2e6a7b72e7d343e6b5e73cc3534f3328925f
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-08-20 12:41:32 +0300

    Create FirstFactorial.js

---
+++ Hard/FirstFactorial.js
@@ -0,0 +1,14 @@
+/*Given is a number number. Return the product of the multiplacement of the individual numbers from 1 upwards.*/
+
+function firstFactorial(number) {
+  let num = number;
+  let fact = number;
+  let i = 1;
+  while (i < number) {
+    i + 1;
+    fact = fact*num-1;
+    num--;
+  }
+
+  return fact:
+}
