36e58b0b1af404c4d9ee0cb422b1757f25d2145e
Sultana <111578356+sultana0423@users.noreply.github.com>
2024-08-24 12:15:50 +0300

    Update CountMe.js

--- Hard/CountMe.js
+++ Hard/CountMe.js
@@ -1,1 +1,13 @@
 /*A String string is given. Return the number of the word "Me" in this string. Count 'Me' only if none 'x' is anywhere in front of it.*/
+function countMe(string) {
+   let count = 0;
+    const words = string.split("Me");
+
+    for (let i = 0; i < words.length - 1; i++) {
+        if (!words[i].includes("x")) {
+            count++;
+        }
+    }
+
+    return count;
+}
